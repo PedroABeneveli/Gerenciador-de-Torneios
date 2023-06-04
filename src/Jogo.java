fdc838b697d1d42539963f2c3d83b2a75dda8b3a
Gustavo Nogueira <112661261+Gubsb@users.noreply.github.com>
2023-06-04 16:45:04 -0300

    Add files via upload

---
+++ src/Jogo.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author gustavo
+ */
+public class Jogo {
+    private String nome;
+    private String criadora;
+    private String publicadora;
+    private Date dataDeCriacao;
+    private ArrayList<String> mapas;
+    private String condicaoVitoria;
+    private ArrayList<Torneio> torneios;
+    
+    public Jogo() {
+        
+    }
+
+    public Jogo(String nome, String criadora, String publicadora, Date dataDeCriacao, ArrayList<String> mapas, String condicaoVitoria, ArrayList<Torneio> torneios) {
+        this.nome = nome;
+        this.criadora = criadora;
+        this.publicadora = publicadora;
+        this.dataDeCriacao = dataDeCriacao;
+        this.mapas = mapas;
+        this.condicaoVitoria = condicaoVitoria;
+        this.torneios = torneios;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCriadora() {
+        return criadora;
+    }
+
+    public void setCriadora(String criadora) {
+        this.criadora = criadora;
+    }
+
+    public String getPublicadora() {
+        return publicadora;
+    }
+
+    public void setPublicadora(String publicadora) {
+        this.publicadora = publicadora;
+    }
+
+    public Date getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(Date dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+
+    public ArrayList<String> getMapas() {
+        return mapas;
+    }
+
+    public void setMapas(ArrayList<String> mapas) {
+        this.mapas = mapas;
+    }
+
+    public String getCondicaoVitoria() {
+        return condicaoVitoria;
+    }
+
+    public void setCondicaoVitoria(String condicaoVitoria) {
+        this.condicaoVitoria = condicaoVitoria;
+    }
+
+    public ArrayList<Torneio> getTorneios() {
+        return torneios;
+    }
+
+    public void setTorneios(ArrayList<Torneio> torneios) {
+        this.torneios = torneios;
+    }
+    
+    
+    
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author gustavo
+ */
+public class Pessoa {
+    protected String nome;
+    protected String email;
+    protected String CPF;
+    protected Date dataDeNascimento;
+    protected String endereco;
+    protected String senha;
+
+    public Pessoa() {
+            
+    }
+    
+    public Pessoa(String nome, String email, String CPF, Date dataDeNascimento, String endereco, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.CPF = CPF;
+        this.dataDeNascimento = dataDeNascimento;
+        this.endereco = endereco;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
