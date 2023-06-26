45cd23a873e1646e3dcc2fd4f028be5511cb1ad7
Gustavo Nogueira <112661261+Gubsb@users.noreply.github.com>
2023-06-26 20:49:46 -0300

    filtro da tela BuscaEquipe

--- src/telas/BuscaEquipe.java
+++ src/telas/BuscaEquipe.java
@@ -5,19 +5,29 @@
  */
 package telas;
 
+import classes.Equipe;
+import classes.Jogador;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author gustavo
  */
 public class BuscaEquipe extends javax.swing.JFrame {
-
+    private ArrayList<Equipe> listaEquipes = ((Jogador) Login.usuarioLogado).getEquipes();
+    
     /**
      * Creates new form BuscaEquipe
      */
     public BuscaEquipe() {
         initComponents();
+        montarTabela(listaEquipes);
     }
 
     /**
@@ -33,6 +43,9 @@ public class BuscaEquipe extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tblEquipes = new javax.swing.JTable();
         btnVoltar = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        btnFiltrar = new javax.swing.JButton();
+        txtFiltrar = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -58,19 +71,27 @@ public class BuscaEquipe extends javax.swing.JFrame {
                 return types [columnIndex];
             }
         });
+        tblEquipes.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblEquipesMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tblEquipes);
-        if (tblEquipes.getColumnModel().getColumnCount() > 0) {
-            tblEquipes.getColumnModel().getColumn(0).setHeaderValue("Nome");
-            tblEquipes.getColumnModel().getColumn(1).setHeaderValue("Vitórias/Derrotas");
-            tblEquipes.getColumnModel().getColumn(2).setHeaderValue("Colocação");
-            tblEquipes.getColumnModel().getColumn(3).setHeaderValue("Pontuação");
-        }
 
         btnVoltar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/exit32px.png"))); // NOI18N
         btnVoltar.setText("Voltar");
         btnVoltar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnVoltarActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Filtrar:");
+
+        btnFiltrar.setText("Filtrar");
+        btnFiltrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnFiltrarActionPerformed(evt);
             }
         });
 
@@ -88,7 +109,13 @@ public class BuscaEquipe extends javax.swing.JFrame {
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 442, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(13, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtFiltrar, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnFiltrar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnVoltar)
                 .addGap(49, 49, 49))
         );
@@ -98,15 +125,39 @@ public class BuscaEquipe extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(65, 65, 65)
-                .addComponent(btnVoltar)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnVoltar)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel2)
+                        .addComponent(txtFiltrar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnFiltrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addContainerGap(58, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void montarTabela(ArrayList<Equipe> listaEquipe) {
+        DefaultTableModel modelo = new DefaultTableModel(new Object[] {"Nome", "Vitórias/Derrotas", "Colocação", "Pontuação"}, 0);
+        
+        for (int i = 0 ; i < listaEquipe.size() ; i++) {
+            Object linha[] = {listaEquipe.get(i).getNome(), 
+                              listaEquipe.get(i).calcularRazaoWinLoss(),
+                              listaEquipe.get(i).getColocacao(),
+                              listaEquipe.get(i).getPontuacao()};
+            modelo.addRow(linha);
+        }
+        
+        tblEquipes.setModel(modelo);
+        
+        tblEquipes.getColumnModel().getColumn(0).setPreferredWidth(50);
+        tblEquipes.getColumnModel().getColumn(1).setPreferredWidth(20);
+        tblEquipes.getColumnModel().getColumn(2).setPreferredWidth(20);
+        tblEquipes.getColumnModel().getColumn(3).setPreferredWidth(20);
+    }
+    
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
         int resp = JOptionPane.showConfirmDialog(null, "Deseja voltar para a tela de jogador?", "Confirmar", JOptionPane.YES_NO_OPTION);
         // 0 = sim, 1 = nao
@@ -116,6 +167,30 @@ public class BuscaEquipe extends javax.swing.JFrame {
             this.dispose();
         }
     }//GEN-LAST:event_btnVoltarActionPerformed
+
+    private void tblEquipesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblEquipesMouseClicked
+        
+    }//GEN-LAST:event_tblEquipesMouseClicked
+
+    private void btnFiltrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFiltrarActionPerformed
+        ArrayList<Equipe> listaFiltrada = new ArrayList<Equipe>();
+        for (Equipe equipe : listaEquipes) {
+            if (equipe.getNome().contains(txtFiltrar.getText())) {
+                listaFiltrada.add(equipe);
+            }
+           
+            if (txtFiltrar.getText().chars().allMatch(Character::isDigit) && Integer.toString(equipe.getColocacao()).contains(txtFiltrar.getText())) {
+                listaFiltrada.add(equipe);
+            }
+            
+            if (txtFiltrar.getText().chars().allMatch(Character::isDigit) && Integer.toString(equipe.getPontuacao()).contains(txtFiltrar.getText())) {
+                listaFiltrada.add(equipe);
+            }
+        }
+        
+        montarTabela(listaFiltrada);
+  
+    }//GEN-LAST:event_btnFiltrarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -153,9 +228,12 @@ public class BuscaEquipe extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnFiltrar;
     private javax.swing.JButton btnVoltar;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblEquipes;
+    private javax.swing.JTextField txtFiltrar;
     // End of variables declaration//GEN-END:variables
 }
